c393a2e88c7cb2dd6c6b5e4ffec5ed6de2ce2763
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-04-23 21:38:19 +0700

    Create BlockDataEventHandler.php

---
+++ src/NhanAZ/BlockData/BlockDataEventHandler.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NhanAZ\BlockData;
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\Listener;
+use pocketmine\nbt\tag\CompoundTag;
+
+class BlockDataEventHandler implements Listener {
+
+	public function __construct(
+		private BlockData $blockData
+	) {
+	}
+
+	/**
+	 * @param BlockBreakEvent $event
+	 * @priority MONITOR
+	 */
+	public function onBlockBreak(BlockBreakEvent $event): void {
+		if (!$event->isCancelled()) {
+			$block = $event->getBlock();
+			$blockData = $this->blockData->getData($block);
+			if ($blockData !== null) {
+				$drops = $event->getDrops();
+				foreach ($drops as $drop) {
+					$compoundTag = new CompoundTag();
+					$compoundTag->setString("blockdata", $blockData);
+					$drop->setCustomBlockData($compoundTag);
+				}
+				$this->blockData->removeData($block);
+			}
+		}
+	}
+
+	/**
+	 * @param BlockPlaceEvent $event
+	 * @priority MONITOR
+	 */
+	public function onBlockPlace(BlockPlaceEvent $event): void {
+		if (!$event->isCancelled()) {
+			$item = $event->getItem();
+			$block = $event->getBlock();
+			$customBlockData = $item->getCustomBlockData();
+			if ($customBlockData !== null) {
+				try {
+					$blockData = $customBlockData->getString("blockdata");
+					if ($blockData !== null) {
+						$this->blockData->setData($block, $blockData);
+					}
+				} catch (\Exception) {
+				}
+			}
+		}
+	}
+}
